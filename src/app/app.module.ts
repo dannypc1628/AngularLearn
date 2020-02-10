2dd2470b35b90569eda279745e51df2a905f08ea
dannyLiu <h152074716@gmail.com>
2020-02-10 21:53:51 +0800

    建立 SocialIconsComponent元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SocialIconsComponent } from './social-icons/social-icons.component';
 
 
 
@@ -12,7 +13,8 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SocialIconsComponent
     ],
   imports: [
     BrowserModule,
