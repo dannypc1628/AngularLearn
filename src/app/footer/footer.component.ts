3afc6145c43189fd01b07effc88983ba879c27c8
dannyLiu <h152074716@gmail.com>
2020-02-12 10:09:09 +0800

    元件型指令與CSS樣式

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  encapsulation: ViewEncapsulation.None //將元件的footer.component.css渲染到全域
 })
 export class FooterComponent implements OnInit {
 
