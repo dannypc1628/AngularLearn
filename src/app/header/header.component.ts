a16f930f76502c228a418f66fcc88d616a9b6432
dannyLiu <h152074716@gmail.com>
2020-02-12 21:20:18 +0800

    練習Input()

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Input()
   title = 'The Will Will Web~~~~';
   url = 'http://blog.miniasp.com/';
   logourl = '/assets/images/logo.png';
